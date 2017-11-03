a0439dbbd8a03ba3592a8ecbcad2fb2ee53b264b
Chandan <chandan.kumar@oodlestechnologies.com>
2017-11-03 15:34:45 +0530

    melody added

---
+++ src/main/java/com/oodles/coreservice/services/AsyncServiceImpl.java
@@ -0,0 +1,29 @@
+package com.oodles.coreservice.services;
+
+import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.core.listeners.DownloadProgressTracker;
+import org.bitcoinj.wallet.Wallet;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+/**
+ *	A service that has methods to do a task asynchronously 
+ *	@author Ajit Soman
+ */
+@Service
+public class AsyncServiceImpl implements AsyncService {
+	/**
+	 * Download block chain Asynchronously and stop once completed
+	 * @param peerGroup
+	 * @param downloadProgressTracker
+	 * @param wallet
+	 * @throws InterruptedException
+	 */
+	@Override
+	@Async
+	public void downloadBlockChainAsync(PeerGroup peerGroup,DownloadProgressTracker downloadProgressTracker,Wallet wallet) throws InterruptedException{
+		peerGroup.startBlockChainDownload(downloadProgressTracker);
+		downloadProgressTracker.await();
+		peerGroup.stopAsync();
+	}
+
+}
